429dde2ce99a84b1d97410d650111d7ae41fe625
Hemant Devda <hemant.devda02@gmail.com>
2024-02-28 14:24:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect, useCallback } from 'react';
 function App() {
+
+  const [length, setLength] = useState(8);
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    let char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=~`';
+    let pass = '';
+    for (let i = 1; i <= length; i++) {
+      pass += char.charAt(Math.floor(Math.random() * char.length));
+    }
+    setPassword(pass);
+
+  }, [length]);
+
+
+  const handleCopy = useCallback(() => {
+  navigator.clipboard.writeText(password);
+  }, [password]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <div className='container'>
+        <div className='head'>
+          <h1>Password generator</h1>
+          <input
+          className='text'
+            type="text"
+            placeholder="password"
+            value={password}
+            readOnly
+          />
+          <button className='text'onClick={handleCopy}>copy</button>
+          <br />
+          <input
+            className='range'
+            type='range'
+            min={8}
+            max={20}
+            value={length}
+            onChange={(e) => { setLength(e.target.value) }}
+          />
+          <label> Length: {length} </label>
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
